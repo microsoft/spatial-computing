dc39b079272f4a3285dab2be216f5b65b70b24b8
Jared Bienz <jbienzms@hotmail.com>
2018-02-15 13:32:16 -0800

    Added button to LuisTester to quickly send test predictions.

---
+++ Assets/MixedRealityAzure-Examples/LUIS/Scripts/Editor/LUISTesterEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LUISTester))]
+public class LUISTesterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (Application.isPlaying)
+        {
+            if (GUILayout.Button("Predict"))
+            {
+                LUISTester tester = (LUISTester)target;
+                tester.luisManager.PredictAndHandle(tester.testUtterence);
+            }
+        }
+    }
+
+}
