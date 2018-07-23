96e27b573487b49f177a485d69a1fc8d88d034e4
Justin Wendlandt <jwendl@hotmail.com>
2018-07-23 11:16:14 -0700

    Initial Commit.

---
+++ Standalone-Samples/Unity-Identity/Assets/Scripts/StartupScript.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupScript
+    : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        var clientId = "";
+        var scopes = new List<string>()
+        {
+            "User.Read",
+        };
+
+        var publicClientApplication = new PublicClientApplication(clientId);
+        var authenticationResult = publicClientApplication.AcquireTokenAsync(scopes).Result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
