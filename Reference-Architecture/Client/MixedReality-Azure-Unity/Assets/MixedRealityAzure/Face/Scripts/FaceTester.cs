9ea8d3c54be8263805b6ccbd9f185d0a17fb8e7d
Ryan Fehr <ryfehr@microsoft.com>
2018-07-24 12:42:49 -0700

    Updated reference to dll

--- Reference-Architecture/Client/MixedReality-Azure-Unity/Assets/MixedRealityAzure/Face/Scripts/FaceTester.cs
+++ Reference-Architecture/Client/MixedReality-Azure-Unity/Assets/MixedRealityAzure/Face/Scripts/FaceTester.cs
@@ -23,7 +23,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using Microsoft.ProjectOxford.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face;;
 using Microsoft.MR.Face; 
 using System.Collections;
 using System.Collections.Generic;
