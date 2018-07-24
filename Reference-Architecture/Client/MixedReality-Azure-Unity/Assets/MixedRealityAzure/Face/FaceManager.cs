24e0bd1ba5487cbd198c500bac963cdf4a09f31b
Vy Ta <vyta@microsoft.com>
2018-07-24 12:28:50 -0700

    Adding Mike's stuff

---
+++ Reference-Architecture/Client/MixedReality-Azure-Unity/Assets/MixedRealityAzure/Face/FaceManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceManager : MonoBehaviour 
+{
+	[SerializeField]
+	private string subscriptionKey = "778e6bec0a3649c6b476ca0749268549";
+	[SerializeField]
+	private string baseUri = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
+
+	private FaceManagerClient watcherClient;
+
+	private void Awake()
+	{
+		watcherClient = new FaceClient(subscriptionKey, baseUri);
+	}
+
+	private void OnDestroy()
+	{
+		DisposeClient();
+	}
+
+	private void DisposeClient()
+	{
+		watcherClient.Dispose();
+	}
+
+	public void SetCredentials(string subscriptionKey, string baseUri)
+	{
+		this.subscriptionKey = subscriptionKey;
+		this.baseUri = baseUri;
+	}
+
+	public async Task<IList<DetectedFace>> Detect(string uri)
+	{
+		return await watcherClient.Detect(uri);
+	}
+
+	public async Task<IList<DetectedFace>> Detect(Stream stream)
+	{
+		return await watcherClient.Detect(stream);
+	}
+}
