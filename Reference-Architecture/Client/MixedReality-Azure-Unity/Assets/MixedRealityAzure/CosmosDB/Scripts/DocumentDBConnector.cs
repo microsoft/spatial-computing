243192b1619c76e1df65c963da7fe242418d8c68
meulta <etienne.margraff@live.fr>
2018-07-25 16:10:38 -0700

    Adding support for CosmosDB SQL: DocumentDB

---
+++ Reference-Architecture/Client/MixedReality-Azure-Unity/Assets/MixedRealityAzure/CosmosDB/Scripts/DocumentDBConnector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// DocumentDB imports
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System;
+using Microsoft.MR;
+
+public class DocumentDBConnector : MonoBehaviour {
+
+    #region Unity Inspector Variables
+    [Tooltip("The Endpoint URL for Cosmos DB in DocumentDB.")]
+    [SecretValue("DocumentDB.EndpointURL")]
+    public string EndpointUrl;
+
+    [Tooltip("The Primary Key for Cosmos DB in DocumentDB.")]
+    [SecretValue("DocumentDB.PrimaryKey")]
+    public string PrimaryKey;
+
+    #endregion
+    
+    void Awake() {
+        ServiceLocator.RegisterService<IDocumentClient, DocumentClient>((sp) => {
+            sp.Add(typeof(IDocumentClient), new DocumentClient(new Uri(EndpointUrl), PrimaryKey));
+        });
+    }
+}
